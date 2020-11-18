bd2594cbe98459186f06cac8db17e72c4095f178
jsantore <jsantore@bridgew.edu>
2020-11-17 20:11:17 -0500

    added a getsize based on reflection

--- Demo1.go
+++ Demo1.go
@@ -5,16 +5,56 @@ import (
 	"reflect"
 )
 
-func basicReflectionDemo(mystery interface{}){
+type profDemo struct {
+	name string
+	id   int
+}
+
+func basicReflectionDemo(mystery interface{}) {
 	mysteryType := reflect.TypeOf(mystery)
 	kind := mysteryType.Kind()
 	fmt.Println("The type of the parameter is: ", mysteryType)
 	fmt.Println("The kind of that parameter is: ", kind)
 }
 
+func makeReflectionMap(number interface{}) interface{} {
+	map1 := make(map[string]int)
+	map2 := make(map[string]float64)
+
+	type1 := reflect.TypeOf(map1)
+	type2 := reflect.TypeOf(map2)
+
+	numberType := reflect.TypeOf(number)
+	if numberType == reflect.TypeOf(4) {
+		return reflect.MakeMap(type1)
+	}
+	return reflect.MakeMap(type2)
+
+}
+
+func getsizeReflection(mysteryData interface{}) int {
+	mysteryType := reflect.TypeOf(mysteryData)
+	kind := mysteryType.Kind()
+	mysteryValue := reflect.ValueOf(mysteryData)
+	switch kind {
+	case reflect.TypeOf(profDemo{}).Kind():
+		return mysteryValue.NumField()
+
+	case reflect.TypeOf([]int{1}).Kind():
+		return mysteryValue.Len()
+	default:
+		return 1
+	}
+}
 
 func main() {
-	var1:=4
+	var1 := 4
 	basicReflectionDemo(var1)
+	var2 := []int{1, 2, 4, 5, 6}
 	fmt.Println("============================")
+	basicReflectionDemo(var2)
+	fmt.Println("============================")
+	var3 := profDemo{name: "John Santore",
+		id: 12345678}
+	basicReflectionDemo(var3)
 }
